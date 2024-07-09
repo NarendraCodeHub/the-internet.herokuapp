661d4019f3f26c9bb2853218ff15ddac52b97469
Narendra122812 <narendrakumar122812@gmail.com>
2024-07-09 23:05:14 +0530

    Modify Geolocation Code

--- Automation Project 1/src/Practice/Geolocation.java
+++ Automation Project 1/src/Practice/Geolocation.java
@@ -4,36 +4,25 @@ import java.util.HashMap;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class Geolocation {
-	public static void main(String[] args)  {
-		//System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
+	public static void main(String[] args) throws InterruptedException  {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		
-		ChromeOptions options = new ChromeOptions();
-		HashMap<String, Integer> contentsettings = new HashMap<String, Integer>();
-		HashMap<String, Object> profile = new HashMap<String, Object>();
-		HashMap<String, Object> prefs = new HashMap<String, Object>();
-		contentsettings.put("geolocation", 1);
-		profile.put("managed_default_content_settings", contentsettings);
-		prefs.put("profile", profile);
-		options.setExperimentalOption("prefs", prefs);
-		WebDriverManager.chromedriver().setup();
-		
-		//options.addArguments("disable-notifications");
-		 
-		//Launch the Browser
-		WebDriver driver = new ChromeDriver(options);
-		
-		//Maximize the window
+        ChromeOptions options = new ChromeOptions();
+        HashMap<String, Object> prefs = new HashMap<>();
+
+        // Automatically allow geolocation permission
+        prefs.put("profile.default_content_setting_values.geolocation", 1);
+      
+        options.setExperimentalOption("prefs", prefs);
+
+        WebDriver driver = new ChromeDriver(options);
+
+  		//Maximize the window
 		driver.manage().window().maximize();
 		
 		//Enter the URL 
@@ -43,6 +32,8 @@ public class Geolocation {
 		driver.findElement(By.linkText("Geolocation")).click();
 		
 		driver.findElement(By.xpath("//button[.='Where am I?']")).click();
+		
+		Thread.sleep(3000);
 		
 		driver.quit();
 	}
